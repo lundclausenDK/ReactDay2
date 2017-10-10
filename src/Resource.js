ec597fadaf5a8c70eacb7227bbbdacb9e98c782e
Mikkel <mikkel@mediagram.dk>
2017-10-10 13:17:53 +0200

    git rework

--- src/Resource.js
+++ src/Resource.js
@@ -3,18 +3,25 @@ import './App.css';
 
 class Resource extends React.Component {
 
-    render() {
+
+    callKoalas() {
         let koalas = ["img/koala01.jpg", "img/koala02.jpg", "img/koala03.jpg"];
 
         let koalaLoop = koalas.map(image => {
-            return <img key={image} src={image} className="koalas" />
+            return <img key={image} src={image} className="koalas"/>
         });
 
+        return koalaLoop;
+    }
+
+    render() {
         return (
             <div>
                 <h1>Hello, this is our new Resources class</h1>
 
-                {koalaLoop}
+                {this.callKoalas()}
+
+
             </div>
         );
 
