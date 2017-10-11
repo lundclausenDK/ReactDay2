a2b7ead39cd64ab3eba4c862ac8c6e21d06d4184
Mikkel <mikkel@mediagram.dk>
2017-10-11 13:19:25 +0200

    work in progress day 4

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //import Gallery from "./Gallery";
-import Converter from "./Converter";
+//import Converter from "./Converter";
+import DataStore from "./DataStore";
 import registerServiceWorker from './registerServiceWorker';
 
 //ReactDOM.render(<Gallery />, document.getElementById('root'));
-ReactDOM.render(<Converter />, document.getElementById('root'));
+//ReactDOM.render(<Converter />, document.getElementById('root'));
+ReactDOM.render(<DataStore />, document.getElementById('root'));
 
 registerServiceWorker();
