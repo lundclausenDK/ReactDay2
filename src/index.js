c6442e4edec774206c0ef6e1fad6cb09b56d41bd
Mikkel <mikkel@mediagram.dk>
2017-10-13 12:05:17 +0200

    Shopping list done and my own shopping list done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import registerServiceWorker from './registerServiceWorker';
 //import Gallery from "./Gallery";
 //import Converter from "./Converter";
-import DataStore from "./DataStore";
-import registerServiceWorker from './registerServiceWorker';
+//import DataStore from "./DataStore";
+//import ShoppingList from "./ShoppingList";
+import ShoppingListCustom from "./ShoppingListCustom";
 
 //ReactDOM.render(<Gallery />, document.getElementById('root'));
 //ReactDOM.render(<Converter />, document.getElementById('root'));
-ReactDOM.render(<DataStore />, document.getElementById('root'));
+//ReactDOM.render(<DataStore />, document.getElementById('root'));
+//ReactDOM.render(<ShoppingList />, document.getElementById("root"));
+ReactDOM.render(<ShoppingListCustom />, document.getElementById("root"));
 
 registerServiceWorker();
