3fbe78bd77790140f57dc5f64e5e4d0e8a7a3eca
Mikkel <mikkel@mediagram.dk>
2017-10-23 20:06:19 +0200

    routing

---
+++ src/Smurfs.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Smurfs = () => (
+    <div>
+        <h1>Smurfs</h1>
+    </div>
+);
+
+export default Smurfs;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 
-import registerServiceWorker from './archive/registerServiceWorker';
+//import registerServiceWorker from './archive/registerServiceWorker';
 //import Gallery from "./Gallery";
 //import Converter from "./Converter";
 //import DataStore from "./DataStore";
 //import ShoppingList from "./ShoppingList";
 //import ShoppingListCustom from "./ShoppingListCustom";
-import RoutingDemo from "./RoutingDemo";
+import MyRouter from "./MyRouter";
 
 //ReactDOM.render(<Gallery />, document.getElementById('root'));
 //ReactDOM.render(<Converter />, document.getElementById('root'));
 //ReactDOM.render(<DataStore />, document.getElementById('root'));
 //ReactDOM.render(<ShoppingList />, document.getElementById("root"));
 //ReactDOM.render(<ShoppingListCustom />, document.getElementById("root"));
-ReactDOM.render(<RoutingDemo />, document.getElementById("root"));
+ReactDOM.render((
 
-registerServiceWorker();
+    <BrowserRouter>
+        <MyRouter/>
+    </BrowserRouter>
+
+), document.getElementById("root"));
+
+//registerServiceWorker();
